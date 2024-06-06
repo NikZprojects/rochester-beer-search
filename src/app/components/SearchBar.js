c9457e38fcc2aeef6a3a2a920ca2cc6da99f24ee
NikZprojects <nikzprojects@gmail.com>
2024-06-06 02:11:19 -0400

    Move "back to breweries" button

--- src/app/components/SearchBar.js
+++ src/app/components/SearchBar.js
@@ -7,6 +7,13 @@ export default function SearchBar({ searchTerm, setSearchTerm, section }) {
         Search Breweries, Beers & Styles
         {section ? ` in Section ${section.toUpperCase()}` : ""}:
       </h1>
+      {section && (
+        <div className="w-100 my-3 text-center">
+          <Link className="text-sky-600" href="/">
+            Back to All Breweries
+          </Link>
+        </div>
+      )}
       <input
         className="mt-3 peer h-full w-full rounded-[7px] border border-blue-gray-200 bg-transparent px-3 py-2.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-sky-500 focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
         type="text"
@@ -17,13 +24,6 @@ export default function SearchBar({ searchTerm, setSearchTerm, section }) {
         value={searchTerm}
         autoFocus
       />
-      {section && (
-        <div className="w-100 mt-8 text-center">
-          <Link className="text-sky-600" href="/">
-            Back to All Beers
-          </Link>
-        </div>
-      )}
     </div>
   );
 }
